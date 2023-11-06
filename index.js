8af43f16b7aaf13ae2af692e412d5db11b6b8d22
gsmith <georgesmith812@gmail.com>
2023-11-05 23:16:25 -0500

    Added operate func to calculate operations with specified operands

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ const add = (first, second) => first + second;
 const subtract = (first, second) => first - second;
 const multiply = (first, second) => first * second;
 const divide = (first, second) => first / second;
+
+let firstOperand = 0;
+let secondOperand;
+let operator;
+
+const operate = (operator, first, second) => {
+    switch(operator){
+        case '+':
+            return add(first, second);
+        case '-':
+            return subtract(first, second);
+        case '*':
+            return multiply(first, second);
+        case '/':
+            return divide(first, second);
+    }
+}
