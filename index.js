b531255e25eb619793a6864da7ef6f6dc7b179f7
gsmith <georgesmith812@gmail.com>
2023-11-05 23:00:55 -0500

    Created add, subtract, multiply and divide func

---
+++ index.js
@@ -0,0 +1,4 @@
+const add = (first, second) => first + second;
+const subtract = (first, second) => first - second;
+const multiply = (first, second) => first * second;
+const divide = (first, second) => first / second;
